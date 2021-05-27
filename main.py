2c43213725556b71bd5dced8e3bc1aeb1976fbbf
Cc17301730 <chaim.chsrak@mail.huji.ac.il>
2021-05-27 20:18:06 +0300

    Produce nice fakes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torchvision.transforms as transforms
 import torch.nn.functional as F
 from tqdm import tqdm
 
+
 ######################### HELPERS ###########################
 def int_to_tuple(x):
     return x if isinstance(x, tuple) else (x, x)
@@ -40,9 +41,11 @@ device = 'cuda'
 class Encoder(nn.Module):
     def __init__(self, resolution=(28, 28), channels=1, compression_size=20):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(channels, 16, (3, 3), stride=(2, 2))
-        self.conv2 = nn.Conv2d(self.conv1.out_channels, self.conv1.out_channels * 2, (3, 3), stride=(2, 2))
-        self.conv3 = nn.Conv2d(self.conv2.out_channels, self.conv2.out_channels * 2, (3, 3), stride=(2, 2))
+        self.conv1 = nn.Conv2d(channels, 16, (4, 4), stride=(2, 2))
+        self.bn1 = nn.BatchNorm2d(self.conv1.out_channels)
+        self.conv2 = nn.Conv2d(self.conv1.out_channels, self.conv1.out_channels * 2, (4, 4), stride=(2, 2))
+        self.bn2 = nn.BatchNorm2d(self.conv2.out_channels)
+        self.conv3 = nn.Conv2d(self.conv2.out_channels, self.conv2.out_channels * 2, (4, 4), stride=(2, 2))
         self.fc1 = nn.Linear(self.conv_width(resolution) ** 2 * self.conv3.out_channels,
                              compression_size)
 
@@ -53,7 +56,7 @@ class Encoder(nn.Module):
     def conv_width(resolution):
         h = resolution[0]
         for _ in range(3):
-            h, _ = conv_out_shape(h, kernel_size=3, stride=2)
+            h, _ = conv_out_shape(h, kernel_size=4, stride=2)
         return h
 
     def forward(self, img):
@@ -61,10 +64,10 @@ class Encoder(nn.Module):
         img = img.to(device)
 
         img1a = self.conv1(img)
-        img2a = self.default_activation(img1a)
+        img2a = self.default_activation(self.bn1(img1a))
 
         img1b = self.conv2(img2a)
-        img2b = self.default_activation(img1b)
+        img2b = self.default_activation(self.bn2(img1b))
 
         img1c = self.conv3(img2b)
         img2c = self.default_activation(img1c)
@@ -77,37 +80,41 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, channels=1, out_resolution=(8, 8), input_size=20):
+    def __init__(self, channels=1, out_resolution=(8, 8), latent_size=20):
         super(Decoder, self).__init__()
         self.out_resolution = out_resolution
-        self.input_size = input_size
-        self.deConv1 = nn.ConvTranspose2d(in_channels=1, out_channels=64, kernel_size=(3, 3), stride=(2, 2),
-                                          padding=(3, 3))
-        self.deConv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(4, 4), stride=(2, 2),
-                                          padding=(1, 1))
-        self.deConv3 = nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=(3, 3), stride=(2, 2))
-        self.deConv4 = nn.ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=(4, 4), stride=(2, 2))
+        self.tConv1 = nn.ConvTranspose2d(in_channels=latent_size, out_channels=64, kernel_size=(4, 4), stride=(1, 1),
+                                         padding=(0, 0))
+        self.tConv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(4, 4), stride=(2, 2),
+                                         padding=(2, 2))
+        self.tConv3 = nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=(4, 4), stride=(2, 2))
+        self.tConv4 = nn.ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=(4, 4), stride=(2, 2),
+                                         padding=(1, 1))
+
+        self.bn1 = nn.BatchNorm2d(self.tConv1.out_channels)
+        self.bn2 = nn.BatchNorm2d(self.tConv2.out_channels)
+        self.bn3 = nn.BatchNorm2d(self.tConv3.out_channels)
+
         self.default_activation = nn.LeakyReLU()
         self.final_activation = nn.Sigmoid()
         self.img_width = 28
 
     def forward(self, z):
         z = z.to(device)
-        img1a = self.deConv1(z)
-        img1b = self.default_activation(img1a)
+        img1a = self.tConv1(z)
+        img1b = self.default_activation(self.bn1(img1a))
 
-        img2a = self.deConv2(img1b)
-        img2b = self.default_activation(img2a)
+        img2a = self.tConv2(img1b)
+        img2b = self.default_activation(self.bn2(img2a))
 
-        img3a = self.deConv3(img2b)
-        img3b = self.default_activation(img3a)
+        img3a = self.tConv3(img2b)
+        img3b = self.default_activation(self.bn3(img3a))
 
-        img4a = self.deConv4(img3b)
+        img4a = self.tConv4(img3b)
         img4b = self.default_activation(img4a)
 
         result = self.final_activation(img4b)
         return result
-
 
 
 @staticmethod
@@ -117,20 +124,19 @@ def __closest_sqr(num):
     return num_
 
 
-loss_1 = lambda x, y: -nn.BCELoss()(x, y)
+mini_max_loss_gen = lambda x, y: -nn.BCELoss()(x, y)
+mini_max_loss_disc = lambda x, y: nn.BCELoss()(x, y)
 
+non_staurating_gen_loss = lambda pred, _: (-.5 * torch.log(pred)).mean()
 
-def loss_2():
-    return 0
-
-
-def loss_3():
-    return 0
-
+ls_loss_disc = lambda pred, label: (.5 * (pred - label) ** 2).mean()
+ls_loss_gen = lambda pred, _: (.5 * (pred - real_label) ** 2).mean()
 
 ######################################################################
+noise_size = 1_00
+######################################################################
 discriminator = Encoder(resolution=(28, 28), compression_size=1).to(device)
-generator = Decoder(channels=1, out_resolution=(28, 28), input_size=20).to(device)
+generator = Decoder(channels=1, out_resolution=(28, 28), latent_size=noise_size).to(device)
 #######################################################################
 disc_optimizer = torch.optim.Adam(discriminator.parameters(), lr=.001)
 gen_optimizer = torch.optim.Adam(generator.parameters(), lr=.001)
@@ -144,35 +150,40 @@ batch_size = 5
 ######################################################################
 fake_label = torch.zeros(size=(batch_size, 1)).to(device)
 real_label = torch.ones(size=(batch_size, 1)).to(device)
-######################################################################
-noise_size = 4
 
 
 ######################################################################
 
 
-def gan_trainer(epochs, k, loss):
+def gan_trainer(epochs, k, loss_gen_method, loss_disc_method):
     for epoch in range(epochs):
         for idx, (img, img_digs) in tqdm(enumerate(data_loader)):
-            z = torch.randn(size=(batch_size, 1, noise_size, noise_size))
             ##################################################
             for inner_idx in range(k):
-                loss_disc = train_discriminator(generator, discriminator, z, img, loss, disc_optimizer)
+                z = get_z(noise_size=100)
+                loss_disc = train_discriminator(generator, discriminator, z, img, loss_disc_method, disc_optimizer)
                 writer.add_scalar('disc_losses', loss_disc, counter.__next__())
-            loss_gen = train_generator(generator, discriminator, z, loss, gen_optimizer)
+            ##########################################################
+            z = get_z(noise_size=100)
+            loss_gen = train_generator(generator, discriminator, z, loss_gen_method, gen_optimizer)
             writer.add_scalar('gen_losses', loss_gen, counter.__next__())
             ###################################################
             if idx % 350 == 1:
                 show_results(generator, img)
 
 
+def get_z(noise_size=100):
+    z = torch.randn(size=(batch_size, noise_size, 1, 1)).to(device)
+    return z
+
+
 def show_results(generator, img):
-    z = torch.randn(size=(1, 1, noise_size, noise_size))
+    z = get_z(100)
     tensor_image = generator(z).squeeze(0)
-    image = torchvision.transforms.ToPILImage()(tensor_image)
+    image = torchvision.transforms.ToPILImage()(tensor_image[0])
     image.show()
     print(image)
-    ###############################
+    ############################################################
     torchvision.transforms.ToPILImage()(img[0]).show()
 
 
@@ -189,19 +200,19 @@ def who_wins(discriminator, generator, get_images, threshold):
 def train_generator(generator, discriminator, z, loss, optimizer):
     img = generator(z)
     pred = discriminator(img)
-    loss_ = loss(pred, fake_label)  # log(1 - D(G(z))) want to minimize
+    loss_ = loss(pred, fake_label)  # log(1 - D(G(z))) want to minimize WANT --- -INF
     optim_step(loss_, optimizer)
     return loss_.item()
 
 
 def train_discriminator(generator, discriminator, z, real_img, loss, optimizer):
     pred_for_real_img = discriminator(real_img)
-    loss_on_real = -loss(pred_for_real_img, real_label)  # log(D(x)) --> want to maximize the loss.
+    loss_on_real = loss(pred_for_real_img, real_label)  # log(D(x)) --> want to maximize the loss. --- WANT 0
     optim_step(loss_on_real, optimizer)
     ###########################################
     fake_img = generator(z)
     pred_for_fake_img = discriminator(fake_img.detach())
-    loss_on_fake = -loss(pred_for_fake_img, fake_label)  # log(1 - D(G(z))) --> want to maximize the loss.
+    loss_on_fake = loss(pred_for_fake_img, fake_label)  # log(1 - D(G(z))) --> want to maximize the loss.  WANT 0
     optim_step(loss_on_fake, optimizer)
     ############################################
     return (loss_on_fake + loss_on_real).item()
@@ -262,4 +273,4 @@ def max_unpool_out_shape(h_w, kernel_size=2, stride=2, pad=0):
 
 
 if __name__ == '__main__':
-    gan_trainer(3, k=20, loss=loss_1)
+    gan_trainer(3, k=1, loss_gen_method=non_staurating_gen_loss, loss_disc_method=mini_max_loss_disc)
